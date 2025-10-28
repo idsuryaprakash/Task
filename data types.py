ecaef2d8979dc7d4edaefd1c8a3b514db148a9b2
idsuryaprakash <id.suryaprakash@gmail.com>
2025-10-28 04:52:22 -0700

    finished

---
+++ data types.py
@@ -0,0 +1,30 @@
+name = "surya pralkash"
+age = 21
+height =58.9
+weight =55
+city ="madurai"
+is_student =True
+marks =[90,98,98,97,99]
+pi=3.4
+school_name="milton school"
+
+print(name)
+print(age)
+print(height)
+print(weight)
+print(city)
+print(is_student)
+print(marks)
+print(pi)
+print(school_name)
+
+print(type(name))
+print(type(age))
+print(type(pi))
+print(type(is_student))
+
+my_number = 10
+my_text = "hello"
+
+print(name,type(my_number))
+print(f"value is {my_text} type is {type(my_text)}")
