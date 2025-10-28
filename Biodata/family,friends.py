6f9abc6cd8fd22cdf4c6f8bfdfe4f81181fd2a4e
idsuryaprakash <id.suryaprakash@gmail.com>
2025-10-28 05:16:06 -0700

    finished

---
+++ Biodata/family,friends.py
@@ -0,0 +1,3 @@
+print("Mother Name is: Sathyavani")
+print("Father Name is: Anbarasu")
+print("Friend's Name is: Sahul,bharath")
---
+++ Biodata/marks.py
@@ -0,0 +1,8 @@
+print("10th Mark")
+
+print("Tamil Mark is: 97")
+print("English Mark is: 88")
+print("Math Mark is: 95")
+print("Science Mark is: 89")
+print("S.Science Mark is: 91")
+print("Total Mark is: 460") 
